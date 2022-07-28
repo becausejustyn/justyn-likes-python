8ef98c14fcabaf50cb26df192a4b4aa62732a8ef
Justyn Rodrigues <77762513+becausejustyn@users.noreply.github.com>
2022-07-28 19:08:34 +1000

    Create misc_helpers.py

---
+++ code/misc_helpers.py
@@ -0,0 +1,10 @@
+import pathlib
+import json
+
+## get package directory ##
+package_dir = pathlib.Path(__file__).parent.parent.resolve()
+
+## load config file ##
+config = None
+with open('{0}/config.json'.format(package_dir), 'r') as fp:
+    config = json.load(fp)
