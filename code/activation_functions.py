a854656abca9c8a65d89abcdebbbffd05f25191a
Justyn Rodrigues <77762513+becausejustyn@users.noreply.github.com>
2022-07-29 13:28:08 +1000

    Update activation_functions.py

--- code/activation_functions.py
+++ code/activation_functions.py
@@ -4,16 +4,33 @@ import math
 
 # activation functions
 
-#Defining a sigmoid function
-def sigmoid(z):
-    return 1/(1 + np.exp(-z)) 
 
-def sigmoid2(x):
-    """
-    evaluate the boltzman function with midpoint xmid and time constant tau
-    over x
-    """
-    return 1. / (1. + np.exp(-x))
+# The sigmoid function which turns any number `x` into a number between 0 and 1
+# np.exp()
+def sigmoid(x: float) -> float:
+    return 1. / (1. + math.exp(-x))
+
+assert sigmoid(0) == 0.5
+
+'''
+xs_sigmoid: List[float] = [x for x in range(-10, 10)]
+ys_sigmoid: List[float] = [sigmoid(x) for x in xs_sigmoid]
+
+plt.plot(xs_sigmoid, ys_sigmoid)
+'''
+
+def simple_softmax(x):
+  exp = math.exp(x)
+  return exp / exp.sum()
+
+def softmax(x):
+    # for batch computation, e.g. softmax([[10, 10], [1, 4]])
+    result = []
+    for instance in x:
+        exp = math.exp(instance - max(instance))
+        #exp = np.exp(instance - np.max(instance))
+        result.append(exp / exp.sum())
+    return result
 
 def logistic(x: float) -> float:
     return 1.0 / (1 + math.exp(-x))
@@ -21,3 +38,18 @@ def logistic(x: float) -> float:
 def logistic_prime(x: float) -> float:
     y = logistic(x)
     return y * (1 - y)
+
+def relu(x):
+    # ReLU(x) = x if (x >= 0), 0 if (x < 0)
+    # ReLU(x) = max(0, x)
+    x[x < 0] = 0
+    return x
+
+'''
+x = np.linspace(-10, 10, 100)
+y = relu(np.linspace(-10, 10, 100))
+fig = plt.figure()
+ax = fig.add_subplot(111, xlabel="x", ylabel="y", title='ReLU')
+ax.plot(x, y, color='skyblue')
+plt.show()
+'''
